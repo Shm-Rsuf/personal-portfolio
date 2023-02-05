77aeba99d7ca080fd65e2e6fe0c1d14598447ab2
shm_usuf <shmusuf@gmail.com>
2023-02-05 20:46:59 +0600

    projects data added

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,3 +1,47 @@
+const data = [
+  {
+    id: 1,
+    title: "Tech Alpha - About a modern e-commerce website",
+    img1: "https://images.pexels.com/photos/3370021/pexels-photo-3370021.jpeg?auto=compress&cs=tinysrgb&w=600",
+    img2: "https://images.pexels.com/photos/2586353/pexels-photo-2586353.jpeg?auto=compress&cs=tinysrgb&w=600",
+    description:
+      "A modern e-commerce website built with React, Redux, TailwindCSS, Redux Thunk, and Redux Toolkit. Features product listing, filtering, checkout and seamless shopping experience. Async actions handled by Redux Thunk and state management made easy with Redux Toolkit.",
+    tools: [
+      "react",
+      "redux",
+      "redux-thunk",
+      "tailwindcss",
+      "redux-toolkit",
+      "e-commerce-website",
+      "GSAP",
+    ],
+    liveLink: "https://www.google.com/",
+    forntEndLink: "https://www.google.com/",
+    backEndLink: "https://www.google.com/",
+  },
+  {
+    id: 2,
+    title: "Foodverse - A modern food delivery online e-commerce website",
+    img1: "https://images.pexels.com/photos/3370021/pexels-photo-3370021.jpeg?auto=compress&cs=tinysrgb&w=600",
+    img2: "https://images.pexels.com/photos/2586353/pexels-photo-2586353.jpeg?auto=compress&cs=tinysrgb&w=600",
+    description:
+      "Foodverse GitHub repository - Source code for the food-focused website Foodverse.com. The site showcases recipes, cooking tips, and engaging food-related articles, all designed to provide a comprehensive and user-friendly experience. Built with HTML, CSS, and JavaScript for a responsive design on all devices. Join us in our mission to explore the",
+    tools: [
+      "html",
+      "react",
+      "redux",
+      "redux-thunk",
+      "tailwindcss",
+      "redux-toolkit",
+      "e-commerce-website",
+      "GSAP",
+    ],
+    liveLink: "https://www.google.com/",
+    forntEndLink: "https://www.google.com/",
+    backEndLink: "https://www.google.com/",
+  },
+];
+
 const Projects = () => {
   return <div>Projects</div>;
 };
