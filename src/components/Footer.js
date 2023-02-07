8f12b77b5c444c5edcad94055b06e32cd0339bd9
shm_usuf <shmusuf@gmail.com>
2023-02-07 14:33:04 +0600

    footer added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,21 @@
+import { HashLink } from "react-router-hash-link";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="footer my-40 text-center text-white/50 overflow-hidden">
+      <p>
+        &copy; {new Date().getFullYear()},{" "}
+        <HashLink
+          smooth
+          to="#home"
+          className="link-item hover:text-white duration-500"
+        >
+          Shah Md. Usuf
+        </HashLink>
+        . Crafted by yours truly
+      </p>
+    </footer>
+  );
 };
 
 export default Footer;
