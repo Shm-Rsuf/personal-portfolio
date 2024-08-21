406f2928a4faf57b7b7cd0ddd1d7de9194a6622d
Usuf Ali <usuf.iu.cse16@gmail.com>
2024-08-21 22:28:11 +0600

    footer changed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,13 +7,13 @@ const Footer = () => {
 
   useGsapFooterReveal(footerRef);
   return (
-    <footer className="footer my-40 text-center text-white/50 overflow-hidden">
+    <footer className='footer my-40 text-center text-white/50 overflow-hidden'>
       <p ref={footerRef}>
         &copy; {new Date().getFullYear()},{" "}
         <HashLink
           smooth
-          to="#home"
-          className="link-item hover:text-white duration-500"
+          to='#home'
+          className='link-item hover:text-white duration-500'
         >
           Usuf Ali
         </HashLink>
